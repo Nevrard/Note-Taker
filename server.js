4f52ea6581bc51735903181f0a01682d9a7ae26a
Nevrard <ndangaevrard@gmail.com>
2021-05-06 18:33:53 -0400

    Procfile creation

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({extended : true}))
