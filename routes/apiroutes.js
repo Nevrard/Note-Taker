7873b2eac4fc5c4e3503ab9d46baa321972eda66
Nevrard <ndangaevrard@gmail.com>
2021-05-06 18:26:57 -0400

    deploying app to heroku

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,11 +1,16 @@
 const path = require('path');
  const fs= require('fs')
+ const crypto = require('crypto')
+
+
   let notesData=[];
  fs.readFile('./db/db.json',(err,data)=>{
     if (err) throw err;
-    notesData = JSON.parse(data);
-    console.log(notesData);
-
+    if(data){
+        notesData = JSON.parse(data);
+        console.log(notesData);
+     }
+    
  });
     
  
@@ -15,22 +20,40 @@ const path = require('path');
 module.exports =  (app)=> {
   // => HTML GET Requests
  // Below code handles when users "visit" a page.
-    
- app.get('/api/notes', (req, res) => res.json(notesData));
+
+  app.get('/api/notes', (req, res) => res.json(notesData));
 
 
- app.post('/api/notes',(req,res) =>{
+  app.post('/api/notes',(req,res) =>{
+                  let newNote=req.body
+                  newNote.id=generateUniqueID()
+                  notesData.push(req.body)
+                
+                  let data = JSON.stringify(notesData)
 
-      notesData.push(req.body)
-      let data = JSON.stringify(notesData)
+                  fs.writeFile('./db/db.json',data,(err) =>{
+                  if (err) throw err;
+                  console.log("Note added successfuly")
+              
+            })
+            res.json(notesData)
+         });
+          function generateUniqueID ()
+          {
+          return crypto.randomBytes(8).toString('hex')
+          }
 
-    fs.writeFile('./db/db.json',data,(err) =>{
-        if (err) throw err;
-        console.log("Note added successfuly")
-        
+app.delete('/api/notes/:note',(req,res) => {
+         let noteID=req.params.note
+          console.log(noteID)
+          for (let i = 0; i < notesData.length; i++) {
+            const currentNote = notesData[i];
+            if (noteID === currentNote.id) {
+                notesData.splice(i,1)
+                }
+
+            }
+            res.json(notesData)
       })
-      res.json(notesData)
- })
-
 
 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
  
 
@@ -12,8 +13,9 @@ const app = express();
 const PORT = 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 require('./routes/htmlroutes')(app)
 
